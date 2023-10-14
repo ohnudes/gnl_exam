e27fe39d1d70ba84fed72a61c297b699f9e21f3f
ohnudes <nivohq@gmail.com>
2023-10-14 18:11:14 +0200

    nmaturan/merge: copypasteando con toda la fuerza: main/splitted into 1 which serves as real prototype, and another one that serves as test. This one doesn't rely on a loop in a main function, prints the content of each string and frees each str memory.

--- src/nmaturan_gnl.c
+++ src/nmaturan_gnl.c
@@ -1,6 +1,57 @@
-
 #include "get_next_line.h"
+#include <stdio.h>
+#include <unistd.h>
 
+/* prototipo para testear:
+* 1. prototipo de read: read de todo el txt en un solo str.
+* 2. do_line devuelve hasta primer salto de linea
+* 3. clean_content devuelve el resto de la linea
+* 4. complete str hace un duplicado despues del read con todo el .txt.
+*/
+
+char	*little_read(char **txt, int fd)
+{
+	char	*content;
+	char	*tmp;
+	int		rbytes;
+
+	tmp = NULL;
+	rbytes = 0;
+	tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	*txt = tmp; // ATENTI! no hacer free en clean_content para mantener el txt vivo!
+	if (!tmp)
+		return (NULL);
+	tmp[BUFFER_SIZE] = '\0';
+	rbytes = read(0, tmp, BUFFER_SIZE);
+	if (rbytes == -1)
+		return (NULL);
+	return (tmp);
+}
+
+char	*mini_gnl(int fd)
+{
+	char		*txt;
+	static char *content = {0};
+	char		*line;
+	char		*eol;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	eol = NULL;
+	line = NULL;
+	if (!content || !ft_strchr(content, '\0'))
+		little_read(&txt, fd);
+	if (content)
+		{}// line = do_line(content, &eol);
+	content = clean_content(content, eol);
+	printf("whole txt: <%s>\nline: <%s>\nclean content: <%s>\n", txt, line, content);
+	free(txt);
+	free(line);
+	free(content);
+	return (line);
+}
+
+// prototipo de main
 char	*test_gnl(int fd)
 {
 	static char *content = {0};
@@ -14,7 +65,8 @@ char	*test_gnl(int fd)
 	if (!content || !ft_strchr(content, '\0'))
 	{}// content = read_to_content(content, &eol)
 	if (content)
-	{}// line = create_line(content, &eol);
+	{}// line = do_line(content, &eol);
 	content = clean_content(content, eol);
 	return (line);
 }
+
